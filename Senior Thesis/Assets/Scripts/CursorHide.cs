c267799772f67505bab52f283143d15fcc9a4d92
Ashley M. Davis <davia966@newschool.edu>
2015-12-04 19:05:12 -0500

    Lock char movement on "esc" or "tab" key down

--- Senior Thesis/Assets/Scripts/CursorHide.cs
+++ Senior Thesis/Assets/Scripts/CursorHide.cs
@@ -1,27 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class CursorHide : MonoBehaviour {
 
 	bool CursorLockedVar;
+
 
 	// Use this for initialization
 	void Start () {
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 		CursorLockedVar = true;
+		GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Cancel") && !CursorLockedVar){
+		if (Input.GetButtonDown ("Cancel") || Input.GetKeyDown (KeyCode.Tab) && CursorLockedVar == false){
 			Cursor.lockState = CursorLockMode.Locked;
 			Cursor.visible = false;
 			CursorLockedVar = true;
-		} else if (Input.GetButtonDown ("Cancel") && !CursorLockedVar) {
+			GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
+
+		} else if (Input.GetButtonDown ("Cancel") || Input.GetKeyDown (KeyCode.Tab) && CursorLockedVar == true) {
 			Cursor.lockState = CursorLockMode.None;
 			Cursor.visible = true;
 			CursorLockedVar = false;
+			GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = false;
 		}
 	}
 }
